1274e0632940274b12e37983acf37f6c2805b1a1
plichtma <plichtma@berkeley.edu>
2022-04-28 19:41:12 -0700

    added popup for when you want to create recipe

--- app/src/main/java/com/example/finalproject/AllRecipes.java
+++ app/src/main/java/com/example/finalproject/AllRecipes.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -64,6 +69,40 @@ public class AllRecipes extends AppCompatActivity {
     /** Called when the user taps on a recipe */
     public void openRecipe(View view) {
         Intent intent = new Intent(this, RecommendedRecipe1.class);
+        startActivity(intent);
+    }
+
+    /**Called when user taps plus button */
+    public void onButtonShowPopupWindowClick(View view) {
+
+        // inflate the layout of the popup window
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.create_recipe_options, null);
+
+        // create the popup window
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true; // lets taps outside the popup also dismiss it
+        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        // show the popup window
+        // which view you pass in doesn't matter, it is only used for the window tolken
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        // dismiss the popup window when touched
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
+    }
+
+    // dismiss the popup window when cancel button clicked
+    public void cancelOptions(View view) {
+        Intent intent = new Intent(this, AllRecipes.class);
         startActivity(intent);
     }
 
